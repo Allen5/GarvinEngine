7af0050a4302a9393a91aefe86c78a9fe7d33b54
Allen.Wu <allenlikeu@gmail.com>
2015-01-17 15:55:46 +0800

    split projects

---
+++ include/util/iniParsor.h
@@ -0,0 +1,30 @@
+
+#ifndef _GARVIN_INIFILEREADER_H_
+#define _GARVIN_INIFILEREADER_H_
+
+#include <public.h>
+#include <fstream>
+#include <config/configData.h>
+
+using namespace GarvinEngine::Config;
+
+namespace GarvinEngine
+{
+	namespace Util
+	{
+		class IniParsor
+		{
+			SINGLETON_DECALRE(IniParsor);
+		public:
+			void listSections(std::ifstream& in,std::vector<std::string> & vecSection);
+			void listKV(std::ifstream& in, const std::string& section, DataTable* table);
+
+		private:
+			void _remSectionFormat(std::string& section);
+			void _addSectionFormat(std::string& section);
+
+		};
+	}
+}
+
+#endif
