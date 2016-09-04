91236605fa23e63ebba1dd444b0e7e71d35d8280
Allen.Wu <allenlikeu@gmail.com>
2016-09-05 01:11:25 +0800

    allen: transfer base header files to C++11

---
+++ base/common.h
@@ -0,0 +1,27 @@
+/**************************************************
+ * filename: common.h
+ * desc: entry point for using
+ * functional: support the basic defination for
+ *             easy using
+ * Authro: Allen.Wu(allenlikeu@gmail.com)
+ **************************************************/
+
+#ifndef __BASE_COMMON_H__
+#define __BASE_COMMON_H__
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <set>
+#include <list>
+#include <assert.h>
+
+// 基础的跨平台类型定义 & 宏定义
+#include <base/types.h>
+#include <base/marcos.h>
+
+#endif//__BASE_COMMON_H__
+
