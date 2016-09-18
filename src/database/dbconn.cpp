57d688033b0a092246e3221dfceb04dfb956af2e
Allen.Wu <allenlikeu@gmail.com>
2016-09-18 16:36:52 +0800

    Allen: delete all. and use cpp 11 for refacting

--- src/database/dbconn.cpp
+++
@@ -1,4 +0,0 @@
-
-#include <database/dbconn.h>
-
-
--- test/src/util/argsParsor_test.cpp
+++
@@ -1,9 +0,0 @@
-
-/**
- * 输入命令行参数个数和char**的值，可分解得到对应的值。
- * 支持获取int, bool, double类型的值
- * @Author : Allen.Wu(allenlikeu@gmail.com)
- * @Date : 2015.05.24
- */
-
-//TODO(allenlikeu@gmail.com):依赖xstring,所以需要先测试xstring
